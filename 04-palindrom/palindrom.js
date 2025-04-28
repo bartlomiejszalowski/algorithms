6e68c7c386d5dfa17714b57be5e57a374d08a792
Bartek <bartlomiejszalowski@gmail.com>
2025-04-28 13:33:52 +0200

    palindrom algoritm

---
+++ 04-palindrom/palindrom.js
@@ -0,0 +1,15 @@
+// --- Directions
+// Given a string, return true if the string is a palindrome
+// or false if it is not.  Palindromes are strings that
+// form the same word if it is reversed.
+
+// --- Examples:
+//   palindrome("abba") === true
+//   palindrome("abcdefg") === false
+
+const isPalindrome = (str) => {
+  const reversed = str.split("").reverse().join("");
+  return str === reversed;
+};
+
+console.log(isPalindrome("abba"));
